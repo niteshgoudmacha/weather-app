52d63841ea2b06938fdca907327bc5aa0802ded8
niteshnitu <niteshgoud7366@gmail.com>
2019-07-19 22:59:00 +0530

    Setting up for deployment.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const API_KEY = process.env.WEATHER_API_KEY
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/' + API_KEY + '/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/' + API_KEY + '/' + latitude + ',' + longitude + '?units=si'
 
     request( { url, json : true }, (error, response) => {
         //console.log(response)
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('unable to fetch weather information.')
         }
         else {
-            callback(undefined, response.body.daily.data[0].summary + 'It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chances of rain')
+            callback(undefined, response.body.daily.data[0].summary + 'It is currently ' + response.body.currently.temperature + ' degrees Celsius. There is a ' + response.body.currently.precipProbability + '% chances of rain')
         }
     })
 }
